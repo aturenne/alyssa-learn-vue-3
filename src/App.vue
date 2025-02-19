ad423f3880c9f2e0a6a7d38a19038bb6a9eb4b1a
alyssaturenne <aturenne22@shschools.org>
2025-02-19 10:39:49 -0600

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+
+  <!-- vbind to interpret imageUrl as variable, not string literal -->
+  <img v-bind:src="imageUrl" alt="" />
+  <br />
+  <!-- short hand (identical to line 4) -->
+  <img :src="imageUrl" alt="" />
+
+  <button @click="changeImg">Change image</button>
+  <hr />
+  <input type="text" :value="defaultInputText" />
+  <hr>
+  <p :class="className">Harry Potter</p>
+
+  <p :class="{ inactive: isInactive, center: isCenter }">
+    Harry Potter
+  </p>
+
+  <p :class="['active','center']">Harry Potter</p>
+
 </template>
 
 <script setup>
+import { ref } from 'vue'
+let message = 'Hello, v-bind!'
 
+let imageUrl = ref('/public/img/banner_1.jpg')
+
+function changeImg(){
+  imageUrl.value = '/public/img/banner_2.jpg'
+}
+
+let defaultInputText = 'Write something here...'
+
+let className = 'active'
+
+// whether the classes are applied is based on these truth values
+let isInactive = ref(false)
+let isCenter = ref(false)
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+img {
+  max-width: 300px;
+}
 
+.active {
+  color: green;
+}
+
+.inactive {
+  color: red;
+  text-decoration: line-through;
+}
+
+.center {
+  text-align: center;
+}
 </style>
