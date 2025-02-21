eb8f0e360a0286aa6123e4d1c896ebbf194072a6
alyssaturenne <aturenne22@shschools.org>
2025-02-21 11:48:16 -0600

    Episode 12

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>v-model vs v-bind</h1>
+  <h2>message: {{ message }}</h2>
+  <div>
+    <p>v-model = v-bind + @input</p>
+    <input
+      type="text"
+      v-bind:value="message"
+      @input="message = $event.target.value"
+    />
+  </div>
+  <div>
+    <!-- simplifies line 6-9 -->
+    <p>v-model</p>
+    <input type="text" v-model="message" />
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message = ref('Hello!')
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
