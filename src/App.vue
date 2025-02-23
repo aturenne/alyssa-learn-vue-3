bf2db5905af5e84d804df119e6bc4b345ef2ba27
alyssaturenne <aturenne22@shschools.org>
2025-02-23 14:04:26 -0600

    Conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>Hello, Vue!</div>
+  <h1>{{ message }}</h1>
+  <p>
+    What has keys but can't open locks, space but no room, and you can enter but
+    can't go outside? What am I?
+  </p>
+
+  <button style="width: 200px" @click="showAnswer = !showAnswer">
+    {{ !showAnswer ? 'Show me the answer!' : 'Hide the answer!' }}
+  </button>
+
+  <!-- if boolean expression is false, v-if element will not even be in the DOM tree -->
+  <!-- real conditional rendering - good for security -->
+  <div v-if="showAnswer">A keyboard.</div>
+
+  <!-- element will always be rendered and remain in DOM tree at all times, it only toggles display element -->
+  <div v-show="showAnswer">A keyboard.</div>
+
+  <!-- 50% chance v-if is rendered, 50% v-else is rendered -->
+  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
+  <div v-else>Now you don't</div> -->
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+let message = ref('Hello, v-if!')
+
+let showAnswer = ref(false)
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style scoped></style>
