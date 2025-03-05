88b4a6bffe6f1ed959a22ab349c6ef33d60608cc
alyssaturenne <aturenne22@shschools.org>
2025-03-04 21:06:22 -0600

    Pass data to a parent component with custom events

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>Hello, Vue!</div>
+  <!-- v-bind to bind an entire post to blogpost component -->
+  <!-- recall: shorthand for binding three properties of post object to three props in BlogPost -->
+  <BlogPost 
+    v-for="post in posts" 
+    :key="posts.id" 
+    v-bind="post"
+    @delete-blog-post="processDeletion"
+  ></BlogPost>
+  <!-- above: we use same name from child $emit to listen to it, call some method-->
 </template>
 
 <script setup>
+import BlogPost from './BlogPost.vue'
+import { ref } from 'vue'
 
+let posts = ref([
+  {
+    id: 1,
+    blogPostTitle: 'What is a muggle in HP world?',
+    blogPostContent:
+      'A muggle is a person who lacks any sort of magical ability...'
+  },
+  {
+    id: 2,
+    blogPostTitle:
+      'HP and the Cursed Child Broadway production suspended until April',
+    blogPostContent:
+      'Adding to the earlier post, the Broadway production of Harry Potter and the Cursed Child has been...'
+  },
+  {
+    id: 3,
+    blogPostTitle: 'Potter DIY: Make Your Own “Harry Potter Puppet Pal”',
+    blogPostContent:
+      'We all remember those iconic YouTube videos of Harry and the gang and probably couldn’t get “The Mysterious...'
+  },
+  {
+    id: 4,
+    blogPostTitle: 'Fan Project Brings Life to “Harry Potter” in Translation',
+    blogPostContent:
+      'It’s no secret that the Harry Potter series is a global phenomenon, having been translated into over 80 languages to date....'
+  }
+])
+
+//can use arguments from the child $emit method
+//second argument from the BlogPost’s emit method will be passed as the first parameter of this method
+function processDeletion(id) {
+  let index = posts.value.findIndex(item=>item.id == id)
+  posts.value.splice(index, 1)
+}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
---
+++ src/BlogPost.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="blog-post">
+        <p>
+            {{ message }}
+        </p>
+        <!-- Display the id, title, and content of a blog post. -->
+        <h2>{{ id }} - {{ blogPostTitle }}</h2>
+        <h4>{{ blogPostContent }}</h4>
+        <!-- BlogPost doesn't have access to post array, so we notify parent what to do -->
+        <!-- first argument: name of event (custom), second argument: which one to delete -->
+        <button @click="$emit('delete-blog-post', id)">Delete post</button>
+        <button @click="emitDeletePostEvent(id)">Delete post</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let message = ref('This is the BlogPost component.')
+defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+
+//optional, but recommended; documents the emits used by this child component
+//required if using function like below
+const emit = defineEmits(['delete-blog-post'])
+
+function emitDeletePostEvent(id) {
+    emit('delete-blog-post', id)
+}
+</script>
+
+<style scoped>
+.blog-post {
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
