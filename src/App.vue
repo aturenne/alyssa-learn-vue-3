6fd29358d831aec1cfb7d1ac0ba003b2267e18d7
alyssaturenne <aturenne22@shschools.org>
2025-03-05 15:48:03 -0600

    Pass multiple template contents with named slots

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Hello, Vue!</div>
+  <BaseLayout>
+    <!-- will replace slot with name header -->
+     <!-- shortcut: #header -->
+    <template v-slot:header>
+      <h1>Here might be a page title.</h1>
+    </template>
+    <!-- will replace slot with name main in child-->
+    <template v-slot:main>
+      <p>
+        A paragraph of the main content.
+      </p>
+      <p>
+        And another one.
+      </p>
+    </template>
+    <template v-slot:footer>
+      <p>Here's some content info.</p>
+    </template>
+  </BaseLayout>
 </template>
 
 <script setup>
-
+import BaseLayout from './BaseLayout.vue';
 </script>
 
 <style lang="scss" scoped>
