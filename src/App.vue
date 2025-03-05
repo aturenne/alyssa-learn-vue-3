cf5d45561db2521a25a0b238016b8e0f94f8ccf9
alyssaturenne <aturenne22@shschools.org>
2025-03-05 16:45:11 -0600

    Vue scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,67 @@
 <template>
-  <div>Hello, Vue!</div>
+  <!-- shorthand for <template v-slot:default="slotProps"-->
+  <StudentList :list="list">
+    <template #default="{ stu }">
+    <span :class="{cursed: stu.name == 'Harry'}">
+    {{ stu.name }}
+  </span>
+  </template>
+  </StudentList>
+
+  <hr />
+
+  <!-- component from Element Plus-->
+  <el-table :data="todoList" stripe border style="width: 100%">
+    <el-table-column prop="userId" label="User ID" width="180" />
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="title" label="Title" />
+    <el-table-column prop="completed" label="Status">
+      <template #default="slotProps">
+        <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
+        <el-tag type="danger" v-else>Incomplete</el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+import StudentList from './StudentList.vue'
 
+const list = ref([
+  {
+    id: 1,
+    name: 'Harry'
+  },
+  {
+    id: 2,
+    name: 'Hermione'
+  },
+  {
+    id: 3,
+    name: 'Ron'
+  }
+])
+
+const todoList = ref([])
+
+//fetch todolist from remote API endpoint
+//receive return data in response
+//if you use await in method body, must define with async
+async function getTodoList() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  const data = await response.json()
+  console.log(data)
+  todoList.value = data
+}
+
+onMounted(() => {
+  getTodoList()
+})
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.cursed {
+  color: red;
+}
 </style>
