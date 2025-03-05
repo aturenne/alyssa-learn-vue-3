f57c6648bb17bec6a9a1641a3baea1e6506ea0f1
alyssaturenne <aturenne22@shschools.org>
2025-03-05 15:24:02 -0600

    Pass template content using slots

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>Hello, Vue!</div>
+  <AlertBox>Invalid username or password. Please try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request time out. Refresh and try again.</h1>
+  </AlertBox>
 </template>
 
 <script setup>
-
+import AlertBox from './AlertBox.vue'
 </script>
 
 <style lang="scss" scoped>
